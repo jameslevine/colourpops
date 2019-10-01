f84571c37c45404404a1441610a29161f00831b4
James Levine <james.levine@ticketmaster.co.uk>
2019-10-01 20:09:51 +0100

    CP-10: Create Components For Adding Entries - Add simple test to check component renders - Create input components with labels to ensure accessibility - Create simple styling which can be added to later. - Create storybook story for component

---
+++ src/components/AddColourForm/AddColourForm.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+
+const AddColourForm = () => {
+  return (
+    <>
+      <StyledRule />
+      <Container>
+        <InputContainer>
+          <StyledLabel htmlFor="colour-name">Colour Name</StyledLabel>
+          <StyledInput placeholder="Enter Colour Name" id="colour-name" />
+        </InputContainer>
+        <InputContainer>
+          <StyledLabel htmlFor="hex-name">Hex Code</StyledLabel>
+          <StyledInput placeholder="Enter HEX Code" id="colour-name" />
+        </InputContainer>
+        <StyledButton>+</StyledButton>
+      </Container>
+    </>
+  );
+};
+
+export default AddColourForm;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  width: 100%;
+  height: 150px;
+  background-color: grey;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledLabel = styled.label`
+  font-size: 15px;
+`;
+
+const StyledRule = styled.hr`
+  width: 50%;
+  border-top: 2px solid black;
+`;
+
+const StyledInput = styled.input`
+  width: 150px;
+  height: 25px;
+  margin-right: 10px;
+  padding: 10px;
+  font-size: 15px;
+  border: 0;
+`;
+
+const StyledButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  background-color: white;
+`;
---
+++ src/stories/AddColourForm.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AddColourForm from "../components/AddColourForm/AddColourForm";
+
+export default {
+  title: "Add Colour Form"
+};
+
+export const AddForm = () => <AddColourForm />;
